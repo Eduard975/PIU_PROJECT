2d06f219a9e23b135371c7cf59dc040c8ec8dc5a
Cosmin Panciuc <cosminpanciuc1@gmail.com>
2025-01-11 22:28:42 +0200

    Add animation to explosion

--- src/main/java/player/Explosion.java
+++ src/main/java/player/Explosion.java
@@ -18,6 +18,8 @@ public class Explosion {
 
     public int explosionFrames = 30;
 
+    public float scaleAnimation = 0.01f;
+
     public Explosion(Vector3f position, float radius) {
         this.position = new Vector3f(position.x, position.y, position.z);
         this.radius = radius;
@@ -53,7 +55,9 @@ public class Explosion {
     public void render() {
         if (explosionFrames > 0) {
             Shader.PROJECTILE.enable();
-            Shader.PROJECTILE.setUniformMat4f("ml_matrix", Matrix4f.translate(position));
+            Shader.PROJECTILE.setUniformMat4f("ml_matrix", Matrix4f.translate(position).multiply(Matrix4f.rotate(-90)).multiply(Matrix4f.scale(scaleAnimation, scaleAnimation, 1.0f)));
+            scaleAnimation += 0.035f;
+            scaleAnimation = scaleAnimation > 1.0f ? 1.0f : scaleAnimation;
             texture.bind();
             mesh.render();
             Shader.PROJECTILE.disable();
